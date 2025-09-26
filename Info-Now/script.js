31f0b8f82e23ea7cef723e4ec64152dcd9af5f52
GuiGmrs <guilhermeguimaraesuft@hotmail.com>
2025-09-26 13:50:29 -0300

    barra de pesquisa funcional

--- Info-Now/script.js
+++ Info-Now/script.js
@@ -3,7 +3,7 @@ const toggle = document.getElementById("darkModeToggle");
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 
-  // Troca o ícone do botão
+  // Só pra trocar o ícone
   if (document.body.classList.contains("dark-mode")) {
     toggle.textContent = "☀️";
   } else {
@@ -15,12 +15,12 @@ const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
 const closeBtn = document.getElementById("closeSidebar");
 
-// ABRIR O SIDEBAR BOCOIÓ
+// ABRIR O SIDEBAR BOCOIÓ clicando na figura
 menuBtn.addEventListener("click", () => {
   sidebar.classList.add("active");
 });
 
-// Fechar sidebar BocoiÓ
+// Fechar sidebar BocoiÓ clicando na figura
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
@@ -31,3 +31,35 @@ document.addEventListener("click", (e) => {
     sidebar.classList.remove("active");
   }
 });
+
+// Elementos que serão filtrados BocoiÓ
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const newsCards = document.querySelectorAll('.news-card');
+
+// Filtrar / Ele puxa oque estiver no <h3> 
+function filtrarNoticias() {
+  const termo = searchInput.value.toLowerCase();
+
+  newsCards.forEach(card => {
+    const titulo = card.querySelector('h3').textContent.toLowerCase();
+    
+    // Mostrar ou esconder
+    if (titulo.includes(termo)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
+
+// CLICANDO
+searchBtn.addEventListener('click', filtrarNoticias);
+
+// ENTER
+searchInput.addEventListener('keyup', function(event) {
+  if (event.key === "Enter") {
+    filtrarNoticias();
+  }
+});
+
